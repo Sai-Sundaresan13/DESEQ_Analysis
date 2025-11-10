5d2c3b854ffa092652c08882a487df0940749f34
K.K Sai Sundaresan <153636249+Sai-Sundaresan13@users.noreply.github.com>
2025-11-10 15:39:49 +0530

    adding clean up methods
    Removed unnecessary rows and columns from counts and metadata files, and updated column names in the metadata.

--- DGE.R
+++ DGE.R
@@ -13,8 +13,22 @@ library(corrplot)
 counts <- read.csv("counts1.csv", header = TRUE, row.names = 1)
 meta <- read.csv("meta1.csv", header = TRUE, row.names = 1)
 
+# Remove unnecessary rows and columns from the counts and the meta file
+counts <- counts[ , -c(1:5)]
+meta <- meta[ , -c(1:12, 14:28)]
+
 # make sure that the rownames in the metadata file is same as the columnnames in the counts file.
 all(rownames(meta) == colnames(counts))
+
+# If the names dont match, change them:
+# Method 1: Using Regular Expressions - 
+colnames(counts) <- sub("_.*", "", colnames(counts))
+
+# Method 2 - Manually Chnage them
+colnames(meta) <- c("GPF", "Stage")
+
+#Method 3 - To assign the rownames of metadata to the column names of the counts or vice verse
+
 
 #Set one of the attributes as a factor for the DESeq2 analysis so that the DGE is done based on the factor. The factor to be selected is one of the column of the metadata file
 meta$Sex <- factor(meta$Sex)   
