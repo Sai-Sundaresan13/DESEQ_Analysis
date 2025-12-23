92a634aa4901162b0252f1a89dc22df14ad2efe6
K.K Sai Sundaresan <153636249+Sai-Sundaresan13@users.noreply.github.com>
2025-12-23 11:02:29 +0530

    Added codes for DESEQ run and separation of significant genes
    Added steps for DESeq2 analysis including filtering, releveling, and plotting results.

--- DGE.R
+++ DGE.R
@@ -31,10 +31,61 @@ colnames(meta) <- c("GPF", "Stage")
 
 
 #Set one of the attributes as a factor for the DESeq2 analysis so that the DGE is done based on the factor. The factor to be selected is one of the column of the metadata file
+#optional before dds creation
 meta$Sex <- factor(meta$Sex)   
 
 # Create a DESeq data set object
 dds <- DESeqDataSetFromMatrix(countData = counts,
 colData = meta,
 design = ~ Sex)
+
+dds
+
+# Filter the genes with less counts
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,]
+dds
+
+# relevel the condition based on the needs
+dds$Group <- relevel(dds$Group, ref = "Control")
+
+# Run ESEQ2
+dds <- DESeq(dds)
+res <- results(dds)
+res
+summary(res)
+
+# Filter the results based on the p-value
+res0.01 <- results(dds, alpha = 0.01)
+summary(res0.01)
+resultsNames(dds)
+
+# Plot MA plots
+plotMA(res)
+plotMA(res0.01)
+
+# Order the results and filter it bsased on p-value 
+resOrdered=res[order(res$pvalue),]
+sourceData=counts(dds,normalized=TRUE)
+resFiltered=subset(resOrdered,resOrdered$pvalue<0.01)
+
+# separate the significant genes and plot heatmap
+colors=colorRampPalette(c("blue","white","red"))(100)
+upRes=resFiltered[resFiltered$log2FoldChange>=2,]
+downRes=resFiltered[resFiltered$log2FoldChange<=-2,]
+
+significantUp=sourceData[match((rownames(upRes)),rownames(sourceData)),]
+significantDown=sourceData[match((rownames(downRes)),rownames(sourceData)),]
+
+pheatmap(significantUp,scale="row",cluster_rows=FALSE,show_rownames=TRUE,cluster_cols=FALSE,col=colors)
+pheatmap(significantDown,scale="row",cluster_rows=FALSE,show_rownames=TRUE,cluster_cols=FALSE,col=colors)
+
+# select the required number of top up and downregulated genes 
+topup20=head(upRes,20)
+topdown20=head(downRes,20)
+top40_combined <- rbind(topup20,topdown20)
+significantgenes=sourceData[match((rownames(top40_combined)),rownames(sourceData)),]
+pheatmap(significantgenes,scale="row",cluster_rows=FALSE,show_rownames=TRUE,cluster_cols=FALSE,col=colors)
+
+
 
